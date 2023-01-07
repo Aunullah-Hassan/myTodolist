34852e96620ad69361b6270e600b5b9c0add56c0
Aunullah Hassan <hassanmdaunullah@gmail.com>
2023-01-07 23:59:39 +0530

    3rd commit:form action route,name attribute and install anduse body-parser as middleware to send form data in request.body object

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express=require('express');
+const bodyParser=require('body-parser');
 const app=express();
 const port=8000;
 
 app.set('view engine','ejs');
 app.set('views','views');
 app.use(express.static('assets'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 todate=Date('12/12/12');
 app.get('/',function(req,res){
@@ -17,6 +19,16 @@ app.get('/',function(req,res){
     });
 
     
+});
+
+app.post('/create_task',function(req,res){
+    console.log('create task requested');
+    return res.redirect('back');
+});
+
+app.get('/delete_task',function(req,res){
+    console.log('delete contact requested');
+    return res.redirect('back');
 });
 
 
